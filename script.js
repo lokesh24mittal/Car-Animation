41f9f23564792f5973477c3942a87c7a6dd87391
Lokesh Mittal <lokesh24mittal@gmail.com>
2021-08-07 19:33:25 +0530

    added htmls

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3');
+audio.setAttribute('src', 'sound/sound.mp3');
 audio.loop=true;
 audio.play()
-// var playPromise = audio.play();
-// if (playPromise !== undefined) {
-//     playPromise.then(_ => {
-//      audio.pause()
-//     })
-//     .catch(error => {
-//       // Auto-play was prevented
-//       // Show paused UI.
-//     });
-//   }
